59b0e5d44a1c0535e9fbd05cb15b3bc3bcd1ef33
SlawekNowy <sosw.slawomir.spiewak@gmail.com>
2024-05-31 14:35:12 +0200

    Update build_luadebug.py
    Point to a for until merge

--- build_scripts/scripts/lua_debug/build_luadebug.py
+++ build_scripts/scripts/lua_debug/build_luadebug.py
@@ -5,7 +5,7 @@ import sys
 deps_dir = sys.argv[1]
 os.chdir(deps_dir)
 
-subprocess.run(["git", "clone", "https://github.com/actboy168/lua-debug"])
+subprocess.run(["git", "clone", "https://github.com/Slaweknowy/lua-debug"])
 os.chdir("lua-debug")
 subprocess.run(["git", "submodule", "init"])
 subprocess.run(["git", "submodule", "update"])
@@ -14,7 +14,7 @@ def reset_to_commit(sha):
 	subprocess.run(["git","fetch"],check=True)
 	subprocess.run(["git","checkout",sha,"--recurse-submodules"],check=True)
 
-reset_to_commit("9e1f46502571c4f25eebfad681312d75399e4cda")
+reset_to_commit("cb69ef831298a62c8eaac278a8a1b05e839c2b74")
 
 subprocess.run(["../luamake/luamake", "lua", "compile/download_deps.lua"])
 subprocess.run(["../luamake/luamake", "-mode", "release"])
