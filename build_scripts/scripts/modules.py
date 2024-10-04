a9cd806251207fbd02d697e7f838a9f66cee9bcd
Sławomir Śpiewak <sosw.slawomir.spiewak@gmail.com>
2024-10-04 16:49:11 +0200

    Update modules.py
    pr_prosper_opengl now properly generates.

--- build_scripts/scripts/modules.py
+++ build_scripts/scripts/modules.py
@@ -11,7 +11,7 @@ os.chdir(modules_dir)
 get_submodule("interfaces","https://github.com/Silverlan/pragma_interfaces.git","48c1b84f2245324e90871924e4f606f846197818")
 get_submodule("pr_audio_dummy","https://github.com/Silverlan/pr_audio_dummy.git","84e1249c296dfc6cf288c8262142170851286f48")
 get_submodule("pr_curl","https://github.com/Silverlan/pr_curl.git","974c67cc76710809a9595fcfbc4167554799cd7f")
-get_submodule("pr_prosper_opengl","https://github.com/Silverlan/pr_prosper_opengl.git","4dcc3045f37da63456f2a3ddcf66490497b87106")
+get_submodule("pr_prosper_opengl","https://github.com/Slaweknowy/pr_prosper_opengl.git","93660d5f823d4b16222f2ee776334401643920c0")
 get_submodule("pr_prosper_vulkan","https://github.com/Silverlan/pr_prosper_vulkan.git","f7416f9d24e79a6cdac2798d1d064c20816cbfee")
 
 os.chdir(curDir)
