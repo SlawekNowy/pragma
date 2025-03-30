76e985d92bdf14dd0c099087897455935f40e7d8
Sławomir Śpiewak <slawekwaga@gmail.com>
2025-03-30 13:07:43 +0200

    Demote doxygen and fix and bump glfw.

--- build_scripts/build.py
+++ build_scripts/build.py
@@ -180,24 +180,23 @@ mkpath(build_dir)
 mkpath(deps_dir)
 mkpath(install_dir)
 mkpath(tools)
-
 ########## clang-19 ##########
 # Due to a compiler bug with C++20 Modules in clang, we have to use clang-19 for now,
 # which is not available in package managers yet.
-if platform == "linux" and (c_compiler == "clang-19" or c_compiler == "clang++-19"):
-	curDir = os.getcwd()
-	os.chdir(deps_dir)
-	clang19_root = os.getcwd() +"/LLVM-19.1.2-Linux-X64"
-	if not Path(clang19_root).is_dir():
-		print_msg("Downloading clang-19...")
-		http_extract("https://github.com/llvm/llvm-project/releases/download/llvmorg-19.1.2/LLVM-19.1.2-Linux-X64.tar.xz",format="tar.xz")
-	if c_compiler == "clang-19":
-		c_compiler = clang19_root +"/bin/clang"
-	if cxx_compiler == "clang++-19":
-		cxx_compiler = clang19_root +"/bin/clang++"
-	print_msg("Setting c_compiler override to '" +c_compiler +"'")
-	print_msg("Setting cxx_compiler override to '" +cxx_compiler +"'")
-	os.chdir(curDir)
+# if platform == "linux" and (c_compiler == "clang-19" or c_compiler == "clang++-19"):
+# 	curDir = os.getcwd()
+# 	os.chdir(deps_dir)
+# 	clang19_root = os.getcwd() +"/LLVM-19.1.2-Linux-X64"
+# 	if not Path(clang19_root).is_dir():
+# 		print_msg("Downloading clang-19...")
+# 		http_extract("https://github.com/llvm/llvm-project/releases/download/llvmorg-19.1.2/LLVM-19.1.2-Linux-X64.tar.xz",format="tar.xz")
+# 	if c_compiler == "clang-19":
+# 		c_compiler = clang19_root +"/bin/clang"
+# 	if cxx_compiler == "clang++-19":
+# 		cxx_compiler = clang19_root +"/bin/clang++"
+# 	print_msg("Setting c_compiler override to '" +c_compiler +"'")
+# 	print_msg("Setting cxx_compiler override to '" +cxx_compiler +"'")
+# 	os.chdir(curDir)
 
 if platform == "linux":
 	os.environ["CC"] = c_compiler
